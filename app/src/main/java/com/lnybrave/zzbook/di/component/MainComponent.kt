a93eed1e202d6992926ce18c70fe2374b65643b1
liunianyong <liunianyon@gmail.com>
2017-08-08 15:55:00 +0800

    hello, main

--- app/src/main/java/com/lnybrave/zzbook/di/component/MainComponent.kt
+++ app/src/main/java/com/lnybrave/zzbook/di/component/MainComponent.kt
@@ -2,15 +2,12 @@ package com.lnybrave.zzbook.di.component
 
 import com.lnybrave.zzbook.di.module.*
 import com.lnybrave.zzbook.di.scope.PerActivity
-import com.lnybrave.zzbook.ui.activity.MainActivity
 import dagger.Component
 
 
 @PerActivity
 @Component(dependencies = arrayOf(AppComponent::class), modules = arrayOf(ActivityModule::class))
 interface MainComponent : ActivityComponent {
-
-    fun inject(mainActivity: MainActivity)
 
     fun plus(bookshelfModule: BookshelfModule): BookshelfComponent
 

6eb4bedf13e847c948b5531f2de36544cf7d2958
lnybrave <lnybrave@gmail.com>
2017-08-30 20:17:30 +0800

    delete BaseBindingAdapter.kt

--- app/src/main/java/com/lnybrave/zzbook/Constants.kt
+++ app/src/main/java/com/lnybrave/zzbook/Constants.kt
@@ -3,24 +3,11 @@ package com.lnybrave.zzbook
 /**
  * Created by lny on 2017/7/29.
  */
-
-
 val DOMAIN = "http://106.15.206.82/"
 
 fun getDomain(): String = DOMAIN
 
-object Constants {
+internal object Constants {
     val TOPIC_SIMPLE = 1
     val TOPIC_COMPLEX = 2
-
-
-}
-
-fun formatBookStatus(status: Int): String {
-    var ret = ""
-    when (status) {
-        0 -> ret = "已完结"
-        1 -> ret = "连载"
-    }
-    return ret
 }
